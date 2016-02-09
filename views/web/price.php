53b94cb1f948b32f5e6ee7701ea4072d1142a7cc
EkaterinaAlekseenko <e.alekseenko@bdgroup.biz>
2016-02-09 22:04:08 +0400

    links

--- views/web/price.php
+++ views/web/price.php
@@ -17,11 +17,11 @@
                 <span class="glyphicon glyphicon-chevron-up"></span>
             </div>
             <div id="hairPrice" class="b-price-table">
-                <div class="b-price-group__title ">
+                <div class="price-toggle-controller b-price-group__title " data-target="#hairPrice1">
                     стрижка
                     <span class="glyphicon glyphicon-chevron-up"></span>
                 </div>
-                <ul>
+                <ul id="hairPrice1">
                     <li class="b-price-table__row">
                         <div class="name">короткие волосы</div>
                         <div class="price">
@@ -61,15 +61,55 @@
                     </li>
 
                 </ul>
-                <div class="b-price-group__title ">
+                <div class="price-toggle-controller b-price-group__title "  data-target="#hairPrice2">
                     сушка
                     <span class="glyphicon glyphicon-chevron-down"></span>
                 </div>
-                <div class="b-price-group__title ">
+                <ul id="hairPrice2">
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+
+                </ul>
+                <div class="price-toggle-controller b-price-group__title " data-target="#hairPrice3">
                     укладка
                     <span class="glyphicon glyphicon-chevron-up"></span>
                 </div>
-                <ul>
+                <ul id="hairPrice3">
                     <li class="b-price-table__row">
                         <div class="name">короткие волосы</div>
                         <div class="price">
@@ -108,27 +148,66 @@
 
                     </li>
                 </ul>
-                <div class="b-price-group__title ">
+                <div class="price-toggle-controller b-price-group__title " data-target="#hairPrice4">
                     маска
                     <span class="glyphicon glyphicon-chevron-down"></span>
                 </div>
+                <ul id="hairPrice4">
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+                    <li class="b-price-table__row">
+                        <div class="name">короткие волосы</div>
+                        <div class="price">
+                            <div><span>1000 руб.</span></div>
+                            <div><span>2000 руб.</span></div>
+                            <div><span>3000 руб.</span></div>
+                        </div>
+
+                    </li>
+                </ul>
             </div>
         </div>
 
 
         <div class="b-price-group">
-            <div class="b-price-group__title b-price-group__title_bold">
-                волосы
+            <div class="price-toggle-controller b-price-group__title b-price-group__title_bold" data-target="#nailPrice">
+                Ногти
                 <span class="glyphicon glyphicon-chevron-up"></span>
             </div>
-            <div class="b-price-table">
-                <div class="b-price-group__title ">
-                    стрижка
-                    <span class="glyphicon glyphicon-chevron-up"></span>
+            <div class="b-price-table" id="nailPrice">
+                <div class="b-price-group__title price-toggle-controller" data-target="#nailPrice1">
+                    Маникюр
+                    <span class="glyphicon glyphicon-chevron-up "></span>
                 </div>
-                <ul>
+                <ul id="nailPrice1">
                     <li class="b-price-table__row">
-                        <div class="name">короткие волосы</div>
+                        <div class="name">Покрытие лаком</div>
                         <div class="price">
                             <div><span>1000 руб.</span></div>
                             <div><span>2000 руб.</span></div>
@@ -137,40 +216,13 @@
 
                     </li>
                     <li class="b-price-table__row">
-                        <div class="name">короткие волосы</div>
+                        <div class="name">Гель-лак</div>
                         <div class="price">
                             <div><span>1000 руб.</span></div>
                             <div><span>2000 руб.</span></div>
                             <div><span>3000 руб.</span></div>
                         </div>
 
-                    </li>
-
-                    <li class="b-price-table__row">
-                        <div class="name">короткие волосы</div>
-                        <div class="price">
-                            <div><span>1000 руб.</span></div>
-                            <div><span>2000 руб.</span></div>
-                            <div><span>3000 руб.</span></div>
-                        </div>
-
-                    </li>
-                    <li class="b-price-table__row">
-                        <div class="name">короткие волосы</div>
-                        <div class="price">
-                            <div><span>1000 руб.</span></div>
-                            <div><span>2000 руб.</span></div>
-                            <div><span>3000 руб.</span></div>
-                        </div>
-
-                    </li>
-                    <li class="b-price-table__row">
-                        <div class="name">Укадка</div>
-                        <div class="price">
-                            <div><span>0</span></div>
-                            <div><span>0</span></div>
-                            <div><span>0</span></div>
-                        </div>
                     </li>
                 </ul>
             </div>
