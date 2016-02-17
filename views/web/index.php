bc58f994943233a6bfc9388afa513559afdc6edb
usov <d.usov@incrdbl.me>
2016-02-17 14:39:19 +0300

    mobile menu

--- views/web/index.php
+++ views/web/index.php
@@ -2,7 +2,7 @@
 use yii\helpers\Url;
 ?>
 <section class="b-menu-image">
-    <div class="b-menu-image-row">
+    <div class="b-menu-image-row  hidden-xs">
         <div class="b-menu-image-item">
             <a href='/web//about/'>
                 <span class="b-menu-image-item__label">&#8212; о салоне &#8212;</span>
@@ -10,7 +10,7 @@ use yii\helpers\Url;
             </a>
         </div>
         <div class="b-menu-image-item">
-            <a href='http://brush-salon.ru'  onclick="this.target='_blank';'">
+            <a href='http://brush-salon.ru' onclick="this.target='_blank';'">
                 <span class="b-menu-image-item__label">&#8212; школа brush &#8212;</span>
                 <img src="<?=Url::to('@web/images/main/school.png')?>">
             </a>
@@ -38,32 +38,42 @@ use yii\helpers\Url;
     <div class="b-menu-image-row">
         <div class="b-menu-image-item">
             <a href='/web/typical/'>
-                <img src="<?=Url::to('@web/images/main/hair.png')?>">
-                <span class="b-menu-image-item__title">волосы</span>
+                <div class="b-menu-image-item__img b-menu-image-item__img_hair"></div>
+                <div class="b-menu-image-item__title">
+                    <span class="title">волосы</span>
+                </div>
             </a>
         </div>
         <div class="b-menu-image-item">
             <a href='/web/typical/'>
-                <img src="<?=Url::to('@web/images/main/nails.png')?>">
-                <span class="b-menu-image-item__title">ногти</span>
+                <div class="b-menu-image-item__img b-menu-image-item__img_nails"></div>
+                <div class="b-menu-image-item__title">
+                    <span class="title">ногти</span>
+                </div>
             </a>
         </div>
         <div class="b-menu-image-item">
             <a href='/web/typical/'>
-                <img src="<?=Url::to('@web/images/main/makeup.png')?>">
-                <span class="b-menu-image-item__title">брови & ресницы & макияж</span>
+                <div class="b-menu-image-item__img b-menu-image-item__img_makeup"></div>
+                <div class="b-menu-image-item__title">
+                    <span class="title">брови & ресницы & макияж</span>
+                </div>
             </a>
         </div>
         <div class="b-menu-image-item">
             <a href='/web/typical/'>
-                <img src="<?=Url::to('@web/images/main/depilation.png')?>">
-                <span class="b-menu-image-item__title">косметология & эпиляция</span>
+                <div class="b-menu-image-item__img b-menu-image-item__img_depilation"></div>
+                <div class="b-menu-image-item__title">
+                    <span class="title">косметология & эпиляция</span>
+                </div>
             </a>
         </div>
         <div class="b-menu-image-item">
             <a href='/web/typical/'>
-                <img src="<?=Url::to('@web/images/main/permanent-makeup.png')?>">
-                <span class="b-menu-image-item__title">пермаментный макияж</span>
+                <div class="b-menu-image-item__img b-menu-image-item__img_permanent-makeup"></div>
+                <div class="b-menu-image-item__title">
+                    <span class="title">пермаментный макияж</span>
+                </div>
             </a>
         </div>
     </div>
